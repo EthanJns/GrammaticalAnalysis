c5862e9c62ff259e9e372ec7ef55265485dea95a
EmilianoFernandez <emilianoqfernandez@gmail.com>
2022-09-07 10:55:56 -0700

    foundation for text analyzer

--- src/main/Main.py
+++ src/main/Main.py
@@ -1,5 +1,13 @@
 from SentenceAnalyzer import SentenceAnalyzer as SA
 from WordAnalyzer import WordAnalyzer as WA
 
+
+#Open the POS file containing words and their parts of speech
+#Open the 10 common sentences text file and store each sentence in a list, removing any punctuation
+#Iterate over that list using the POS_Dictionary to create a version of the sentence that is only parts of speech 
+pos_test_file = open('./resources/pos_test.txt','r', encoding='latin1')    #Open Pos File 
+sentences_file = open('./resources/10_common_english_sentences.txt','r')
 wa = WA()
-sa = SA(wa)
+sa = SA(wa,pos_test_file)
+sa.analyze_text(sentences_file)
+print("working")   
--- src/main/SentenceAnalyzer.py
+++ src/main/SentenceAnalyzer.py
@@ -2,8 +2,16 @@ from WordAnalyzer import WordAnalyzer as WA
 
 class SentenceAnalyzer:
     
-    def __init__(self, wa):
+    def __init__(self, wa, pos_file):
         self.wa = wa
+        self.pos_dict = {}
+        pos_text = pos_file.readlines()
+        for text in pos_text:
+            #print(text)
+            pos_split = text.split(":")
+            self.pos_dict[pos_split[0]] = pos_split[1][:-1]
+            
+        print(self.pos_dict)
 
     def analyze_sentence(self, sentence):
         return 0
@@ -11,5 +19,15 @@ class SentenceAnalyzer:
     def analyze_sentences(self, sentences):
         return 0
 
-    def analyze_text(self, text_file):
+    def analyze_text(self, sentences_file):
+        sentences_pos = open('./resources/sentences_list','w+')
+        sentences = sentences_file.readlines()
+        for sentence in sentences:
+            print(sentence)
+            new_sentence = sentence.strip() + ' : '
+            for word in sentence.strip().split(" "):
+                new_sentence = new_sentence + self.pos_dict[word.lower().strip()]+" "
+            sentences_pos.write(new_sentence+"\n")
+        sentences_pos.close()
+
         return 0
