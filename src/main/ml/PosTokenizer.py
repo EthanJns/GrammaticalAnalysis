7c3ea5be5ba39ade4b4952815593bcf02982f255
EthanJns <ethanjns.io@gmail.com>
2022-09-09 15:56:10 -0500

    More testing

--- src/main/ml/PosTokenizer.py
+++ src/main/ml/PosTokenizer.py
@@ -1,4 +1,7 @@
-
+#The following class will be used to tokenize/vectorize out data
 class PosTokenizer:
     def __init__():
         print("This class will be used to tokenize sentences into parets of speech into vectorized data for our nn")
+
+    def vectorize_input_data(self, sentences):
+        print()
